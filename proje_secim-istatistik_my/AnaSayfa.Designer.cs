c89ff4348618a68fab4d76372f9b50eb26b3cd71
Ali Kurtulmus <mkurtulmus.eng@gmail.com>
2022-08-27 12:17:56 +0300

    Add project files.

---
+++ proje_secim-istatistik_my/AnaSayfa.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace proje_secim_istatistik_my
+{
+    partial class AnaSayfa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtad = new System.Windows.Forms.TextBox();
+            this.txta = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtb = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtc = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtd = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txte = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnoy = new System.Windows.Forms.Button();
+            this.btngra = new System.Windows.Forms.Button();
+            this.btncikis = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İlçe Ad:";
+            // 
+            // txtad
+            // 
+            this.txtad.Location = new System.Drawing.Point(107, 122);
+            this.txtad.Name = "txtad";
+            this.txtad.Size = new System.Drawing.Size(306, 26);
+            this.txtad.TabIndex = 1;
+            // 
+            // txta
+            // 
+            this.txta.Location = new System.Drawing.Point(107, 174);
+            this.txta.Name = "txta";
+            this.txta.Size = new System.Drawing.Size(306, 26);
+            this.txta.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "A Partisi";
+            // 
+            // txtb
+            // 
+            this.txtb.Location = new System.Drawing.Point(107, 226);
+            this.txtb.Name = "txtb";
+            this.txtb.Size = new System.Drawing.Size(306, 26);
+            this.txtb.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "B Partisi";
+            // 
+            // txtc
+            // 
+            this.txtc.Location = new System.Drawing.Point(107, 278);
+            this.txtc.Name = "txtc";
+            this.txtc.Size = new System.Drawing.Size(306, 26);
+            this.txtc.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 281);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "C Partisi";
+            // 
+            // txtd
+            // 
+            this.txtd.Location = new System.Drawing.Point(107, 330);
+            this.txtd.Name = "txtd";
+            this.txtd.Size = new System.Drawing.Size(306, 26);
+            this.txtd.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 333);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "D Partisi";
+            // 
+            // txte
+            // 
+            this.txte.Location = new System.Drawing.Point(107, 382);
+            this.txte.Name = "txte";
+            this.txte.Size = new System.Drawing.Size(306, 26);
+            this.txte.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 385);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "E Partisi";
+            // 
+            // btnoy
+            // 
+            this.btnoy.Location = new System.Drawing.Point(107, 425);
+            this.btnoy.Name = "btnoy";
+            this.btnoy.Size = new System.Drawing.Size(96, 56);
+            this.btnoy.TabIndex = 12;
+            this.btnoy.Text = "Oy Ver";
+            this.btnoy.UseVisualStyleBackColor = true;
+            this.btnoy.Click += new System.EventHandler(this.btnoy_Click);
+            // 
+            // btngra
+            // 
+            this.btngra.Location = new System.Drawing.Point(209, 425);
+            this.btngra.Name = "btngra";
+            this.btngra.Size = new System.Drawing.Size(99, 56);
+            this.btngra.TabIndex = 13;
+            this.btngra.Text = "Grafikler";
+            this.btngra.UseVisualStyleBackColor = true;
+            this.btngra.Click += new System.EventHandler(this.btngra_Click);
+            // 
+            // btncikis
+            // 
+            this.btncikis.Location = new System.Drawing.Point(314, 425);
+            this.btncikis.Name = "btncikis";
+            this.btncikis.Size = new System.Drawing.Size(99, 56);
+            this.btncikis.TabIndex = 14;
+            this.btncikis.Text = "Çıkış";
+            this.btncikis.UseVisualStyleBackColor = true;
+            this.btncikis.Click += new System.EventHandler(this.btncikis_Click);
+            // 
+            // AnaSayfa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 580);
+            this.Controls.Add(this.btncikis);
+            this.Controls.Add(this.btngra);
+            this.Controls.Add(this.btnoy);
+            this.Controls.Add(this.txte);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtd);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtc);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtb);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txta);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtad);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "AnaSayfa";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtad;
+        private System.Windows.Forms.TextBox txta;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtb;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtc;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtd;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txte;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnoy;
+        private System.Windows.Forms.Button btngra;
+        private System.Windows.Forms.Button btncikis;
+    }
+}
+
---
+++ proje_secim-istatistik_my/AnaSayfa.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace proje_secim_istatistik_my
+{
+    public partial class AnaSayfa : Form
+    {
+        public AnaSayfa()
+        {
+            InitializeComponent();
+        }
+
+        //Data Source=DESKTOP-QBFPK40\SQLEXPRESS;Initial Catalog=yb_secim;Integrated Security=True
+        SqlConnection baglanti=new SqlConnection("Data Source=DESKTOP-QBFPK40\\SQLEXPRESS;Initial Catalog=yb_secim;Integrated Security=True");
+        
+        private void btnoy_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("insert into parti_tablo (ilcead,partia,partib,partic,partid,partie) values (@p1,@p2,@p3,@p4,@p5,@p6)", baglanti);
+            komut.Parameters.AddWithValue("@p1", txtad.Text);
+            komut.Parameters.AddWithValue("@p2", txta.Text);
+            komut.Parameters.AddWithValue("@p3", txtb.Text);
+            komut.Parameters.AddWithValue("@p4", txtc.Text);
+            komut.Parameters.AddWithValue("@p5", txtd.Text);
+            komut.Parameters.AddWithValue("@p6", txte.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Oy giriş işlemi tamamlandı");
+
+
+        }
+
+        private void btngra_Click(object sender, EventArgs e)
+        {
+            Grafikler gr = new Grafikler();
+            gr.Show();
+        }
+
+        private void btncikis_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
